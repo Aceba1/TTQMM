d97138fc4483c6780cc83d57f4c57504cfe14885
AlexejheroYTB <alexejhero@gmail.com>
2018-08-10 21:06:01 +0300

    Moved some namespaces to their own files

--- QModManager/Utility.cs
+++ QModManager/Utility.cs
@@ -1,4 +1,5 @@
 ﻿// Code from https://github.com/ahk1221/SMLHelper/blob/2.0/SMLHelper/Utility/ReflectionHelper.cs
+using System;
 using System.Reflection;
 
 namespace QModManager.Utility
@@ -166,6 +167,35 @@ namespace QModManager.Utility
                     fieldInfo.SetValue(copy, value);
                 }
             }
+        }
+    }
+
+    public static class ExceptionUtils
+    {
+        public static void OutputInnerExceptionRecursively(Exception e)
+        {
+            if (e.InnerException != null)
+            {
+                Console.WriteLine("Inner exception:");
+                Console.WriteLine(e.InnerException.Message);
+                Console.WriteLine(e.InnerException.StackTrace);
+                OutputInnerExceptionRecursively(e.InnerException);
+            }
+        }
+
+        public static void ParseException(Exception e, bool exit = true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Uh-oh. An exception has occurred. This is not a good thing.");
+            Console.WriteLine("Please take a screenshot and open a bug report on nexus");
+            Console.WriteLine();
+            Console.WriteLine(e.Message);
+            Console.WriteLine(e.StackTrace);
+            OutputInnerExceptionRecursively(e);
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            if (exit) Environment.Exit(0);
         }
     }
 }
